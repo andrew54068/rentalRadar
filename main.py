c2851d06263d5b32840c20d4c11db5b71afc5064
andrew54068 <andrew0424718012@yahoo.com.tw>
2020-05-27 11:02:18 +0800

    [feat] add loop.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # env
 from dotenv import load_dotenv
 # load_dotenv(verbose=True)
+import time
 
 from DataBaseConnector import DataBaseConnector
 import DataProvider as DataProvider
@@ -9,6 +10,8 @@ db = DataBaseConnector()
 # db.alter_table()
 # db.drop_table()
 # db.clear_table()
-url = "https://rent.591.com.tw/?kind=0&region=1&order=posttime&orderType=desc"
-subjects = DataProvider.get_subjects_from_url(url)
-db.update_subject(subjects)
+while 1:
+    url = "https://rent.591.com.tw/?kind=0&region=1&order=posttime&orderType=desc"
+    subjects = DataProvider.get_subjects_from_url(url)
+    db.update_subject(subjects)
+    time.sleep(10 * 60)
