aacec4de42d5b4bc53af3cdf0a99e478cd2d76c5
andrew54068 <andrew0424718012@yahoo.com.tw>
2020-06-13 22:47:43 +0800

    [feat] add upload token api.

--- app.py
+++ app.py
@@ -3,13 +3,30 @@ from flask import Flask, request, jsonify
 from DataBaseConnector import DataBaseConnector
 from preference import Preference
 import sys
+import Crawler as Crawler
 
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    db = DataBaseConnector()
+    # Crawler.start_crawl(db)
+    app.run(host='127.0.0.1', debug=True)
 
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+
+@app.route('/api/v1/user/token', methods=['POST'])
+def get_user_token():
+    request_data = request.json
+    device_token = request_data.get('device_token')
+
+    if device_token is not None and type(device_token) is str:
+        return jsonify({'message': "success"}), 200
+    else:
+        return jsonify({'error': "device_token not provided or is not string"}), 400
 
 
 @app.route('/api/v1/user/preference', methods=['POST'])
@@ -35,8 +52,3 @@ def get_tasks():
         return jsonify({'error': "request body is not array."}), 400
 
     return jsonify({'message': "success"}), 200
-
-
-if __name__ == '__main__':
-    db = DataBaseConnector()
-    app.run(host='127.0.0.1', debug=True)
