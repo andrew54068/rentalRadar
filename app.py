aba595b7181af53dad290a3eedd7efbabb0d1914
andrew54068 <andrew0424718012@yahoo.com.tw>
2020-06-13 19:52:58 +0800

    [refactor] refactor api end point.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 
 from DataBaseConnector import DataBaseConnector
 from preference import Preference
+import sys
 
 app = Flask(__name__)
 
@@ -13,30 +14,27 @@ def index():
 
 @app.route('/api/v1/user/preference', methods=['POST'])
 def get_tasks():
-    request_data = request.get_json()
-    print("request_data: ")
-    print(request_data)
-    user_id = request_data['user_id']
-    filter_type = request_data['filter_type']
-    value = request_data['filter_value']
+    request_data = request.json
+    if isinstance(request_data, list):
+        prs = []
+        for element in request_data:
+            user_id = element.get('user_id')
+            filter_type = element.get('filter_type')
+            value = element.get('filter_value')
 
-    if not user_id:
-        return jsonify({'error': "not user_id provided."}), 400
-    if not filter_type:
-        return jsonify({'error': "not filter_type provided."}), 400
-    if not value:
-        return jsonify({'error': "not filter_value provided."}), 400
+            if not user_id:
+                return jsonify({'error': "user_id in one of element not provided."}), 400
+            if not filter_type:
+                return jsonify({'error': "filter_type in one of element not provided."}), 400
+            if not value:
+                return jsonify({'error': "filter_value in one of element not provided."}), 400
+            prs.append(Preference(user_id, filter_type, value))
 
-    print(f"user_id: {user_id}")
-    print(f"filter_type: {filter_type}")
-    print(f"value: {value}")
+        db.update_user_preference(prs)
+    else:
+        return jsonify({'error': "request body is not array."}), 400
 
-    pr = Preference(user_id, filter_type, value)
-    db.update_user_preference([pr])
-
-    # exec(open("./main.py").read())
-
-    return jsonify({}), 200
+    return jsonify({'message': "success"}), 200
 
 
 if __name__ == '__main__':
