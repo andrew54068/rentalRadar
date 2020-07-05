e026700ae352a393736645164b665542a6cd81c0
andrew54068 <andrew0424718012@yahoo.com.tw>
2020-07-05 22:35:31 +0800

    [refactor]

--- PasswordChecher.py
+++ PasswordChecher.py
@@ -9,13 +9,16 @@ class PasswordChecher:
         self.db = db
 
     def check(self, email, password):
-        (id, hashed_password) = self.db.get_user_id(email)
-        if bcrypt.checkpw(str(password).encode('utf-8'), str(hashed_password).encode('utf-8')):
-            print("It Matches!")
-            return id
-        else:
-            print("It Does not Match :(")
+        id_with_hash = self.db.get_user_id_with_password_hash(email)
+        if id_with_hash is None:
             return None
+        else:
+            if bcrypt.checkpw(str(password).encode('utf-8'), str(id_with_hash[1]).encode('utf-8')):
+                print("It Matches!")
+                return id_with_hash[0]
+            else:
+                print("It Does not Match :(")
+                return None
 
     def hash_password(self, password):
         return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
